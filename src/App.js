2603b894816ddd0418074cd99a6e5b248f02116d
Elijahscriptdev <elijahobominuru@gmail.com>
2020-09-09 13:56:21 +0100

    add about-us and contact-us routes and pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import HomePage from './pages/homepage/homepage';
 import SignInPage from './pages/sign-in/sign-in';
 import SignUpPage from './pages/sign-up/sign-up';
 import './App.css';
+import AboutUS from './pages/about-us/about-us';
+import ContactUs from './pages/contact-us/contact-us';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
             <Route exact path='/' component={ HomePage } />
             <Route path='/sign_in' component={ SignInPage } />
             <Route path='/sign_up' component={ SignUpPage } />
+            <Route path='/contact-us' component={ ContactUs } />
+            <Route path='/about-us' component={ AboutUS } />
           </Switch>
         </BrowserRouter>
     </div>
--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import './homepage.css';
+// import { Header } from 'react-bootstrap/lib/Modal';
 
 const HomePage = () => (
     <div className='homepage'>
-      <h1>Team-065A-Frontend</h1>
+
+      <header>
+        <h1>Team-065A-Frontend</h1>
+      </header>
+
+      <main>
+
+      </main>
+
+      <footer>
+
+      </footer>
+
     </div>
   );
 
